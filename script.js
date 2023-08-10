9bfaae2d76543ca843f701c7dcd135792747eff1
Frederic Merkl <frederic@frederics-mbp.localdomain>
2023-08-10 23:13:29 +0200

    created interface that keeps track of score of player and cpu. created a message area that anounces the winner of the round and tells the player what the cpu chose.

--- script.js
+++ script.js
@@ -1,72 +1,212 @@
-        const cpuChoice = function randomSelection() {
-            const options = ["rock", "paper", "scissors"];
-            const result = Math.floor(Math.random() * options.length);
-            return options[result]
-          }
-      
-      
-      
-
+let playerScore = 0;
+let cpuScore = 0;
         
+        
+        
+        
+        
+        
+const cpuChoice = function randomSelection() {
+    const options = ["rock", "paper", "scissors"];
+    const result = Math.floor(Math.random() * options.length);
+    return options[result]
+          }
+
+
+const updatePlayerScore = function () {
+        const scoreBoardPlayer = document.querySelector(".playerScoreBoard .score");
+        scoreBoardPlayer.textContent = `${playerScore}`
+         };
+
+const updateCpuScore = function () {
+        const scoreBoardPlayer = document.querySelector(".cpuScoreBoard .score");
+        scoreBoardPlayer.textContent = `${cpuScore}`
+
+        };
+
 
         const playRound = function playRound (playersChoice, cpuChoice) { 
-            //userInput = prompt("Enter rock, paper or scissors")
-           //playersChoice = playersChoice.toLowerCase()
-          //console.log(cpuChoice())
-              if (playersChoice == "Rock") {
-                    console.log("You chose rock")
-                    if (cpuChoice == "rock") {
-                        console.log("CPU chose rock");
-                        return("Its a draw, no points");
-                    }
-                    else if (cpuChoice == "paper") {
-                        console.log("CPU chose paper");
-                     //   cpuScore++;
-                        return "Paper beats rock, you lose!";
-                    }   
-                    else if (cpuChoice == "scissors") {
-                        console.log("CPU chose scissors");
-                      //  playersScore++;
-                        return "Rock beats scissors, you WIN!";
-                    }
 
-                } else if( playersChoice == "Paper") {
-                    console.log("You chose paper")
-                    if (cpuChoice == "rock") {
-                        console.log("CPU chose rock")
-                     //   playersScore++;
-                        return "Paper beats rock, you win!";
-                    }
-                    else if (cpuChoice == "paper") {
-                        console.log("CPU chose paper")
-                        return "Its a draw!";
-                    }
-                    else if (cpuChoice == "scissors") {
-                        console.log("CPU chose scissors")
-                       // cpuScore++;
-                        return "Scissor beats paper, CPU wins!";
-                    }
-                } else if (playersChoice == "Scissors") {
-                    console.log("You chose scissors")
-                    if (cpuChoice == "rock") {
-                        console.log("CPU chose rock")
-                       // cpuScore++;
-                        return "Rock beats scissors, you lose!";
-                    }
-                    else if (cpuChoice == "paper") {
-                        console.log("CPU chose paper")
-                       // playersScore++;
-                        return "Scissor beats paper, you win!";
-                    }
-                    else if (cpuChoice == "scissors") {
-                        console.log("CPU chose scissors")
-                        return "Scissors, its a draw!";
+            let announcement = document.querySelector(".announcement");  
+                
+            if (playersChoice == "Rock") {
+   
+                announcement.innerText = "You chose rock";    
+                    
+
+            if (cpuChoice == "rock") {
+                setTimeout(() => {
+                    announcement.innerText = "Computer chose rock";  
+
+                }, 1500);
+                       
+                setTimeout(() => {
+                    announcement.innerText = "Its a draw!";
+
+                }, 2500)
+                }
+                else if (cpuChoice == "paper") {
+                    setTimeout(() => { 
+                        announcement.innerText = "Computer chose paper";
+                    }, 1500);        
+                    
+                    setTimeout(() => { 
+                        announcement.innerText = "Paper beats rock. You lose!";
+                    }, 2500); 
+                    
+                    cpuScore++;
+                    setTimeout(()=> {
+                        updateCpuScore()
+                    }, 3500)    
+                    }   
+                else if (cpuChoice == "scissors") {
+                    setTimeout(() => { 
+                        announcement.innerText = "Computer chose Scissor";
+                    }, 1500); 
+
+                    setTimeout(() => { 
+                        announcement.innerText = "Rock beats Scissors. You win!";
+                    }, 2500); 
+
+                    playerScore++;
+                    setTimeout(()=> {
+                        updatePlayerScore()
+                    }, 3500) 
+                    
                     }
                 }
-                
-        }
 
+            else if (playersChoice == "Paper") {
+   
+                announcement.innerText = "You chose paper";    
+                        
+    
+                if (cpuChoice == "rock") {
+                    
+                    setTimeout(() => {
+                        
+                        announcement.innerText = "Computer chose rock";  
+    
+                    }, 1500);
+                           
+                    setTimeout(() => {
+                        announcement.innerText = "Paper shatters rock...lol. You Win!";
+    
+                    }, 2500)
+                    playerScore++;
+                    setTimeout(()=> {
+                        updatePlayerScore()
+                    }, 3500)    
+                    }   
+                    
+                    else if (cpuChoice == "paper") {
+                        setTimeout(() => { 
+                            announcement.innerText = "Computer chose paper.";
+                        }, 1500);        
+                        
+                        setTimeout(() => { 
+                            announcement.innerText = "Its a draw!";
+                        }, 2500); 
+                        
+                        
+                            
+                        }   
+                    else if (cpuChoice == "scissors") {
+                        setTimeout(() => { 
+                            announcement.innerText = "Computer chose scissor";
+                        }, 1500); 
+    
+                        setTimeout(() => { 
+                            announcement.innerText = "Scissor slices paper. You lose!";
+                        }, 2500); 
+    
+                        cpuScore++;
+                        setTimeout(()=> {
+                            updateCpuScore()
+                        }, 3500)    
+                        }}
+                        
+                    
+
+                    else if (playersChoice == "Scissors") {
+   
+                        announcement.innerText = "You chose scissors";    
+                            
         
+                        if (cpuChoice == "rock") {
+                        setTimeout(() => {
+                            announcement.innerText = "Computer chose rock";  
+        
+                        }, 1500);
+                               
+                        setTimeout(() => {
+                            announcement.innerText = "You get crushed by rock and die!";
+        
+                        }, 2500)
+                        cpuScore++
+                        setTimeout(()=> {
+                            updateCpuScore()
+                        }, 3500)    
+                        } 
+                        
+                        else if (cpuChoice == "paper") {
+                            setTimeout(() => { 
+                                announcement.innerText = "Computer chose paper";
+                            }, 1500);        
+                            
+                            setTimeout(() => { 
+                                announcement.innerText = "Scissors cuts paper. You win!";
+                            }, 2500); 
+                            
+                            playerScore++;
+                            setTimeout(()=> {
+                                updatePlayerScore()
+                            }, 3500)    
+                            }   
+                            
+                           
+                        else if (cpuChoice == "scissors") {
+                            setTimeout(() => { 
+                                announcement.innerText = "Computer chose Sscissors";
+                            }, 1500); 
+        
+                            setTimeout(() => { 
+                                announcement.innerText = "draw!";
+                            }, 2500); 
+        
+                            
+                            }}}
+                        
+                    
+
+
+                    
+
+
+   /* if (playerScore || cpuScore < 5) {
+        return;
+         } else {
+            
+
+
+          } */
+
+          /*function stopGame () {
+            let announcement = document.querySelector(".announcement");  
+            
+            setTimeout( ()=> {
+            
+                if(playerScore == 5) {
+                        announcement.textContent = "YOU WON!!"
+            
+                } else if (cpuScore == 5) {
+            
+                    announcement.textContent = "CPU won!"
+                }
+                }, 2000)    
+            
+                 } 
+        */
      
 
 
@@ -81,8 +221,7 @@ buttons.forEach(button => {
     button.addEventListener("click",() => {
         const playersChoice = button.value;
         const result = playRound(playersChoice, cpuChoice())
-       // console.log(playRound(playersChoice, cpuChoice()))
-        console.log(result)
+      
         
     });
 })
@@ -138,3 +277,12 @@ buttons.forEach(button => {
          } else {alert("It is a draw")}
 
      } */
+
+
+
+
+     // NOTES got later
+
+     // implement power up that allow you to win against 1 more opponent
+     // change RPS to something else. eg ork, knight, elve
+
