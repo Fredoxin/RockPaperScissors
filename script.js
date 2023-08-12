2304acfc34fb0b71486a965563bf9c7ac14f624f
Frederic Merkl <frederic@frederics-mbp.localdomain>
2023-08-12 16:19:28 +0200

    added block buttons function. The User can not click the button while the function runs anymore.

--- script.js
+++ script.js
@@ -6,17 +6,18 @@ let announcement = document.querySelector(".announcement");
 ///FUNCTION EXPRESSION I USE FOR THE GAME////   
 
 const gameEnd = function () {
-    let announcement = document.querySelector(".announcement");
+
     if (playerScore == 5) {
         setTimeout(()=>{
-            console.log("gameEndWON")
+            
             announcement.textContent = "YOU WON THE GAME";
-        }, 0)
+        }, 500)
     }  else if (cpuScore == 5) {
         setTimeout(()=>{
-            console.log("gameEndLOST")
+            
             announcement.textContent = "YOU LOST THE GAME";
-        }, 0)
+
+        }, 500)
     }
 };
         
@@ -44,22 +45,22 @@ const updateCpuScore = function () {
             gameEnd();
             } 
         };
-
+// THE GAME
 const playRound = function (playersChoice, cpuChoice = cpu) { 
  
    if (playersChoice == cpuChoice) {                                                // EVENT HANDLING WHEN OUTCOME IS A DRAW.
-        setTimeout(() => {                                                          // display players choice.
+        setTimeout(() => {                                                          // displays players choice.
             announcement.textContent = `You chose ${playersChoice}`;               
                 setTimeout(()=>{
-                    announcement.textContent = `CPU chose ${cpuChoice}`;             //display cpu choice.
+                    announcement.textContent = `CPU chose ${cpuChoice}`;             //displays cpu choice.
                         setTimeout(()=>{
                             announcement.textContent = "It a draw!";                //display round outcome
                             setTimeout(()=> {
                                 announcement.textContent = "Choose your weapon for your next battle"; 
-                            }, 1000)
-                        }, 500)
+                            }, 1500)
+                        }, 1200)
 
-                },1000)
+                },1200)
 
         }, 100) 
    }
@@ -79,9 +80,9 @@ const playRound = function (playersChoice, cpuChoice = cpu) {
                                     else {
                                     announcement.textContent = "Choose your weapon for your next battle"; 
                                     }
-                                }, 1000);
-                            }, 500);
-                    }, 1000); 
+                                }, 1500);
+                            }, 1200);
+                    }, 1200); 
             }, 100);
 
             }
@@ -101,9 +102,9 @@ const playRound = function (playersChoice, cpuChoice = cpu) {
                                     else {
                                     announcement.textContent = "Choose your weapon for your next battle";
                                 }
-                                }, 1000);
-                            }, 500);
-                    }, 1000); 
+                                }, 1500);
+                            }, 1200);
+                    }, 1200); 
             }, 100);
         }
          
@@ -116,269 +117,58 @@ const playRound = function (playersChoice, cpuChoice = cpu) {
 
 const buttons = document.querySelectorAll("button");                                        // creates node-list of all buttons
 
-    buttons.forEach(button => {
+ buttons.forEach(button => {
         button.addEventListener("click",() => {                                             // adds eventlistener "click" to each button and assigns 
                                                                                             // the value of the button to the playerChoice variable
             let playersChoice = button.value;
             
-            let cpu = cpuChoice();                                                              
+           // let cpu = cpuChoice();                                                              
             
             if (playerScore < 5 && cpuScore < 5) {
                 
-              playRound(playersChoice, cpu)                                                 // runs the game as long score is not 5.
-
+              playRound(playersChoice, cpuChoice())                                                 // runs the game as long score is not 5 and the player clicks a button
+              disableButtons()
             }
            
             
-    });
+    });    
 });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
+function disableButtons () {                // DISABLES AND ENABLES THE BUTTON FOR THE TIME THE GAME RUNS.
+    buttons.forEach(button =>{
+        button.disabled = true;
 
        
-           /*
-           
-            if (playersChoice == "Rock") {
-   
-                announcement.innerText = "You chose rock";    
-                    
+      setTimeout(() => {
+        button.disabled = false;
+      }, 4000)
 
-            if (cpuChoice == "rock") {
-                setTimeout(() => {
-                    announcement.innerText = "Computer chose rock";  
+    })
 
-                }, 1500);
-                       
-                setTimeout(() => {
-                    announcement.innerText = "Its a draw!";
+}
 
-                }, 2500)
-                }
-                else if (cpuChoice == "paper") {
-                    
-                    setTimeout(() => { 
-                        announcement.innerText = "Computer chose paper";
-                    }, 1500);        
-                    
-                    setTimeout(() => { 
-                        announcement.innerText = "Paper beats rock. You lose!";
-                    }, 2500); 
-                    
-                    cpuScore++;
-                    setTimeout(()=> {
-                        updateCpuScore()
-                    }, 3500)    
-                    }   
-                else if (cpuChoice == "scissors") {
-                    setTimeout(() => { 
-                        announcement.innerText = "Computer chose Scissor";
-                    }, 1500); 
 
-                    setTimeout(() => { 
-                        announcement.innerText = "Rock beats Scissors. You win!";
-                    }, 2500); 
 
-                    playerScore++;
-                    setTimeout(()=> {
-                        updatePlayerScore()
-                    }, 3500) 
-                    
-                    }
-                }
 
-            else if (playersChoice == "Paper") {
-   
-                announcement.innerText = "You chose paper";    
-                        
-    
-                if (cpuChoice == "rock") {
-                    
-                    setTimeout(() => {
-                        
-                        announcement.innerText = "Computer chose rock";  
-    
-                    }, 1500);
-                           
-                    setTimeout(() => {
-                        announcement.innerText = "Paper shatters rock...lol. You Win!";
-    
-                    }, 2500)
-                    playerScore++;
-                    setTimeout(()=> {
-                        updatePlayerScore()
-                    }, 3500)    
-                    }   
-                    
-                    else if (cpuChoice == "paper") {
-                        setTimeout(() => { 
-                            announcement.innerText = "Computer chose paper.";
-                        }, 1500);        
-                        
-                        setTimeout(() => { 
-                            announcement.innerText = "Its a draw!";
-                        }, 2500); 
-                        
-                        
-                            
-                        }   
-                    else if (cpuChoice == "scissors") {
-                        setTimeout(() => { 
-                            announcement.innerText = "Computer chose scissor";
-                        }, 1500); 
-    
-                        setTimeout(() => { 
-                            announcement.innerText = "Scissor slices paper. You lose!";
-                        }, 2500); 
-    
-                        cpuScore++;
-                        setTimeout(()=> {
-                            updateCpuScore()
-                        }, 3500)    
-                        }}
-                        
-                    
 
-                    else if (playersChoice == "Scissors") {
-   
-                        announcement.innerText = "You chose scissors";    
-                            
-        
-                        if (cpuChoice == "rock") {
-                        setTimeout(() => {
-                            announcement.innerText = "Computer chose rock";  
-        
-                        }, 1500);
-                               
-                        setTimeout(() => {
-                            announcement.innerText = "You get crushed by rock and die!";
-        
-                        }, 2500)
-                        cpuScore++
-                        setTimeout(()=> {
-                            updateCpuScore()
-                        }, 3500)    
-                        } 
-                        
-                        else if (cpuChoice == "paper") {
-                            setTimeout(() => { 
-                                announcement.innerText = "Computer chose paper";
-                            }, 1500);        
-                            
-                            setTimeout(() => { 
-                                announcement.innerText = "Scissors cuts paper. You win!";
-                            }, 2500); 
-                            
-                            playerScore++;
-                            setTimeout(()=> {
-                                updatePlayerScore()
-                            }, 3500)    
-                            }   
-                            
-                           
-                        else if (cpuChoice == "scissors") {
-                            setTimeout(() => { 
-                                announcement.innerText = "Computer chose Sscissors";
-                            }, 1500); 
-        
-                            setTimeout(() => { 
-                                announcement.innerText = "draw!";
-                            }, 2500);   
-                            }}}
-                        
-                    
+
+// you can disable buttons with button.diabled.
+// find a method that allowes execution for a certain amount of time.
+// write a function that set disables atributes of all button to disabled true and after 4 seconcd sets it on false again
+
+/*
+function blockButtons () {
+    console.log()
+    buttons.forEach(button =>{
+    button.removeEventListener("click", addEvent)
+})
+}
 */
-
-                    
-
-
-   /* if (playerScore || cpuScore < 5) {
-        return;
-         } else {
-            
-
-
-          } */
-
-          /*function stopGame () {
-            let announcement = document.querySelector(".announcement");  
-            
-            setTimeout( ()=> {
-            
-                if(playerScore == 5) {
-                        announcement.textContent = "YOU WON!!"
-            
-                } else if (cpuScore == 5) {
-            
-                    announcement.textContent = "CPU won!"
-                }
-                }, 2000)    
-            
-                 } 
-        */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       /* let playersScore = 0;
-        let cpuScore = 0;
-        i = 0;
-        
-
-
-       window.onload = function game()   {
-        
-        while (i < 5) {
-
-        let userInput = prompt("Enter rock, paper or scissors")
-        userInput = userInput.toLowerCase()
-        if (userInput != "rock" && userInput != "paper" && userInput != "scissors") {
-            alert("invalid Input - Enter rock, paper or scissor")
-        } else {
-        console.log(playRound(userInput, cpuChoice = randomSelection())) §
-        i++ }
-        }
-
-    
-        if(cpuScore < playersScore) {
-            alert("You Win")
-        }
-         else if(cpuScore > playersScore) {
-            alert("CPU wins");
-         } else {alert("It is a draw")}
-
-     } */
-
-
-
 
      // NOTES got later
 
      // implement power up that allow you to win against 1 more opponent
      // change RPS to something else. eg ork, knight, elve
-
